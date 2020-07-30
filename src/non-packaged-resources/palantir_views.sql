c845addc8f2c8502f6ca7be9e762f52f99f2cdb3
eichmann <david-eichmann@uiowa.edu>
2020-07-30 16:20:29 +0000

    Support in place for non-InCommon institution users coming in through login.gov.

--- src/non-packaged-resources/palantir_views.sql
+++ src/non-packaged-resources/palantir_views.sql
@@ -7,15 +7,8 @@ SELECT
     registration.last_name,
     registration.institution,
     registration.orcid_id,
-    registration.gsuite_email,
-    registration.slack_id,
-    registration.github_id,
-    registration.twitter_id,
     registration.expertise,
     registration.therapeutic_area,
-    registration.assistant_email,
-    registration.enclave,
-    registration.workstreams,
     registration.created,
     registration.updated
    FROM n3c_admin.registration
@@ -23,6 +16,8 @@ SELECT
   	registration.enclave
   AND
   	institution != 'NIH'
+  AND
+  	(official_institution is null OR official_institution != 'login.gov')
   AND
   	institution NOT IN (SELECT incommon from n3c_admin.registration_remap)
 UNION
@@ -32,17 +27,10 @@ SELECT
     registration.official_last_name,
     registration.first_name,
     registration.last_name,
-    registration_remap.ror as instutition,
+    registration_remap.ror as institution,
     registration.orcid_id,
-    registration.gsuite_email,
-    registration.slack_id,
-    registration.github_id,
-    registration.twitter_id,
     registration.expertise,
     registration.therapeutic_area,
-    registration.assistant_email,
-    registration.enclave,
-    registration.workstreams,
     registration.created,
     registration.updated
    FROM n3c_admin.registration, n3c_admin.registration_remap
@@ -57,17 +45,30 @@ SELECT
     registration.official_last_name,
     registration.first_name,
     registration.last_name,
-    nih_ic.title,
+    institutionname as institution,
     registration.orcid_id,
-    registration.gsuite_email,
-    registration.slack_id,
-    registration.github_id,
-    registration.twitter_id,
     registration.expertise,
     registration.therapeutic_area,
-    registration.assistant_email,
-    registration.enclave,
-    registration.workstreams,
+    registration.created,
+    registration.updated
+   FROM n3c_admin.registration, n3c_admin.registration_domain_remap, n3c_admin.feedout
+  WHERE
+  	registration.enclave
+  AND
+  	substring(email from '@(.*)$')=email_domain
+  AND
+  	registration_domain_remap.ror=feedout.institutionid
+UNION
+SELECT
+ 	registration.email,
+    registration.official_first_name,
+    registration.official_last_name,
+    registration.first_name,
+    registration.last_name,
+    nih_ic.title as institution,
+    registration.orcid_id,
+    registration.expertise,
+    registration.therapeutic_area,
     registration.created,
     registration.updated
    FROM n3c_admin.registration, nih_foa.nih_ic
@@ -89,4 +90,26 @@ SELECT
   		SELECT nih_ic.title FROM nih_foa.nih_ic where nih_ic.ic IN (select substring(official_full_name from '/([^)]+)') FROM n3c_admin.registration WHERE enclave and institution = 'NIH')
   		UNION
   		SELECT ror from n3c_admin.registration_remap
+  		UNION
+  		SELECT institutionname from n3c_admin.feedout,n3c_admin.registration_domain_remap where registration_domain_remap.ror=feedout.institutionid
+  		));
+
+CREATE VIEW palantir.n3c_organization2 AS
+SELECT
+ 	organization.id,
+    organization.name,
+    duaexecuted as dua_signed,
+    organization.wikipedia_url
+   FROM ror.organization, n3c_admin.feedout
+  WHERE (
+  	organization.id = feedout.institutionid
+  AND
+  	organization.name IN ( 
+  		SELECT registration.institution FROM n3c_admin.registration WHERE enclave and institution != ' NIH'
+  		UNION
+  		SELECT nih_ic.title FROM nih_foa.nih_ic where nih_ic.ic IN (select substring(official_full_name from '/([^)]+)') FROM n3c_admin.registration WHERE enclave and institution = 'NIH')
+  		UNION
+  		SELECT ror from n3c_admin.registration_remap
+  		UNION
+  		SELECT institutionname from n3c_admin.feedout,n3c_admin.registration_domain_remap where registration_domain_remap.ror=feedout.institutionid
   		));
